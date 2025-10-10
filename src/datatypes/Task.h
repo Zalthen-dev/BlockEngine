5464f3d0f00410a1cc3758ce18407e412e958e18
Zalthen <zalthen@protonmail.com>
2025-10-09 19:25:27 -0700

    Update Task.h

--- src/datatypes/Task.h
+++ src/datatypes/Task.h
@@ -8,9 +8,9 @@
 #include "raylib.h"
 #include "raymath.h"
 
-#include "../../luau/VM/include/lua.h"
-#include "../../luau/VM/include/lualib.h"
-#include "../../luau/Compiler/include/luacode.h"
+#include "../../dependencies/luau/VM/include/lua.h"
+#include "../../dependencies/luau/VM/include/lualib.h"
+#include "../../dependencies/luau/Compiler/include/luacode.h"
 
 struct LuaTask {
     lua_State* thread;
