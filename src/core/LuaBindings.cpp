f0a1e074c179970d16727bd4197d69dde663213e
Zalthen <zalthen@protonmail.com>
2025-10-11 23:22:28 -0700

    Update LuaBindings.cpp

--- src/core/LuaBindings.cpp
+++ src/core/LuaBindings.cpp
@@ -1,5 +1,24 @@
 #include "LuaBindings.h"
 
+std::vector<std::string> luaOutput;
+Logger logger;
+
+int lua_CapturedPrint(lua_State* L) {
+    int n = lua_gettop(L);
+    std::ostringstream oss;
+
+    for (int i = 1; i <= n; ++i) {
+        size_t len;
+        const char* s = luaL_tolstring(L, i, &len);
+        oss << s;
+        if (i < n) oss << "\t";
+        lua_pop(L, 1);
+    }
+
+    logger.Log(oss.str());
+    return 0;
+}
+
 static int l_Signal_Connect(lua_State* L) {
     Signal* sig = *(Signal**)luaL_checkudata(L, 1, "Signal");
     luaL_checktype(L, 2, LUA_TFUNCTION);
@@ -21,7 +40,6 @@ static int l_Signal_DisconnectAll(lua_State* L) {
     return 0;
 }
 
-
 void Lua_RegisterSignal(lua_State* L) {
     luaL_newmetatable(L, "Signal");
 
@@ -33,30 +51,10 @@ void Lua_RegisterSignal(lua_State* L) {
     lua_pop(L, 1);
 }
 
-
 namespace LuaBindings {
-    std::vector<BasePart*>* g_instances = nullptr;
-    Camera3D* gg_camera = nullptr;
-
-    int Lua_SetCameraPos(lua_State* L) {
-        float x = (float)lua_tonumber(L, 1);
-        float y = (float)lua_tonumber(L, 2);
-        float z = (float)lua_tonumber(L, 3);
-
-        gg_camera->position = {x, y, z};
-
-        return 0;
-    }
-
-    void RegisterScriptBindings(lua_State* L, std::vector<BasePart*>& parts, Camera3D& g_camera) {
-        g_instances = &parts;
-        gg_camera = &g_camera;
-
-        lua_newtable(L);
-
-        lua_pushcfunction(L, Lua_SetCameraPos, "SetCameraPos"); lua_setfield(L, -2, "SetCameraPos");
-
-        lua_setglobal(L, "Engine");
+    void RegisterScriptBindings(lua_State* L) {
+        lua_pushcfunction(L, lua_CapturedPrint, "print");
+        lua_setglobal(L, "print");
 
         Vector3Game_Bind(L);
         Color3_Bind(L);
